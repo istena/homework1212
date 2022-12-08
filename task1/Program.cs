8b186fd9dd785a15bbad7411ef1abccec04413dc
Ilya <sannikov444@yandex.ru>
2022-12-08 11:28:48 +0300

    begin work

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a<(b+c)&&b<(a+c)&&c<(a+b))  Console.Write("есть такое треугольник");
+else Console.Write("Нет такого треугольника ");
+
